6c11c58ab29e5d72014d428576607eb664d14811
pqm <patrickqmurphy@gmail.com>
2019-10-01 18:14:57 -0500

    done

---
+++ api_key_store.py
@@ -0,0 +1,10 @@
+# OpenWeatherMap API Key
+weather_api_key = "a2d84f55c1eed8f01f6a9a29bcbdfe31"
+
+# Google API Key
+g_key = "AIzaSyC0JKyaZIIA0O1MQ8U5Nb0M1m5drHdbnFU"
+
+# Google URLs
+geo = "https://maps.googleapis.com/maps/api/geocode/json"
+nearby = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
